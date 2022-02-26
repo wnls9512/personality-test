42aeaa049e68441af4b300e94435192dee381bad
wnls9512 <wwoon9512@gmail.com>
2022-02-26 23:13:33 +0900

    commit

---
+++ src/main/java/com/pjj/personalityTest/PersonalityTestApplication.java
@@ -0,0 +1,13 @@
+package com.pjj.personalityTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalityTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjj/personalityTest/PersonalityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjj.personalityTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
